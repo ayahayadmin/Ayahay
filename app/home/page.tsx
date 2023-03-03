0273f5a4f4d567d7385f05878469e79867a6499f
Chenesyy <chesternevansy@gmail.com>
2023-03-03 23:26:37 +0800

    Add header incomplete

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,6 +1,9 @@
+import Header from "@/common/components/Header";
+
 export default function Home() {
   return (
     <div>
+      <Header />
       <h1>Hello Home</h1>
     </div>
   );
---
+++ common/components/Header.tsx
@@ -0,0 +1,57 @@
+'use client';
+import React from 'react';
+import { CaretDownOutlined } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Button, Menu } from 'antd';
+import styles from './Header.module.css';
+
+type MenuItem = Required<MenuProps>['items'][number];
+
+const getItem = (
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuItem[],
+  type?: 'group'
+): MenuItem => {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  } as MenuItem;
+};
+
+const items: MenuProps['items'] = [
+  getItem('Home', 'home'),
+  getItem('Destinations', 'destinations'),
+  getItem('About us', 'aboutUs'),
+  getItem('My Account', 'myAccount', <CaretDownOutlined />, [
+    getItem('Sub Menu 1', 'subMenu1'),
+    getItem('Sub Menu 2', 'subMenu2'),
+  ]),
+  getItem('Contact', 'contact'),
+];
+
+export default function Header() {
+  return (
+    <div className={styles.containerFluid}>
+      <div className={styles.header}>
+        <div className={styles.logo}>
+          <a href='https://www.google.com'>
+            <img src='/assets/ayahay_logo.png' alt='Ayahay Logo' />
+          </a>
+        </div>
+        <Menu mode='horizontal' defaultSelectedKeys={['1']} items={items} />
+        <div>
+          <Button type='primary' size='large'>
+            Book Now
+          </Button>
+          <Button size='large'>Sign In</Button>
+          <Button size='large'>Register</Button>
+        </div>
+      </div>
+    </div>
+  );
+}
