7175b7d529a63d93207ebd6c6a1608b37651bdfb
Chenesyy <chesternevansy@gmail.com>
2023-03-05 03:19:11 +0800

    Footer incomplete

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,10 +1,12 @@
-import Header from "@/common/components/Header";
+import Footer from '@/common/components/Footer';
+import Header from '@/common/components/Header';
 
 export default function Home() {
   return (
     <div>
       <Header />
       <h1>Hello Home</h1>
+      <Footer />
     </div>
   );
 }
---
+++ common/components/Footer.tsx
@@ -0,0 +1,11 @@
+'use client';
+import React from 'react';
+import styles from './Footer.module.css';
+
+export default function Footer() {
+  return (
+    <div className={styles.containerFluid}>
+      <h1>Footer</h1>
+    </div>
+  );
+}
