adad527ba4ea9b20812ef0907c58ee27dc08e867
Carlos <ngocarlos@gmail.com>
2023-03-05 15:36:50 +0800

    revise home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,91 +1,69 @@
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
-import styles from './page.module.css';
-
-const inter = Inter({ subsets: ['latin'] });
+import TripSearchQuery from "@/common/components/TripSearchQuery";
+import Port from "@/models/port";
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href='https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            By{' '}
-            <Image
-              src='/vercel.svg'
-              alt='Vercel Logo'
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const portNames = [
+    "Ilolo",
+    "Bacolod",
+    "Batangas",
+    "Baybay, Leyte",
+    "Bogo, Cebu",
+    "Bato, Leyte",
+    "Butuan",
+    "Cagayan de Oro",
+    "Calapan",
+    "Calbayog City",
+    "Caticlan",
+    "Cebu",
+    "Consuelo, Camotes",
+    "Danao",
+    "Dapitan",
+    "Dapdap",
+    "Dumaguete",
+    "Dipolog",
+    "Dapa, Siargao",
+    "EB MagaIona,  Negros Occidental",
+    "Estancia",
+    "Getafe",
+    "Guimaras",
+    "Hagnaya",
+    "lligan",
+    "Isabel, Leyte",
+    "Jagna, Bohol",
+    "Medellin, Cebu",
+    "Larena, Siquijor",
+    "Cataingan, Masbate",
+    "Masbate",
+    "Matnog",
+    "Manila",
+    "Nasipit",
+    "Odiongan, Romblon",
+    "Ormoc",
+    "Ozamiz",
+    "Ozamiz",
+    "Palompon",
+    "Plaridel",
+    "Puerto Princesa, Palawan",
+    "Puerto Galera",
+    "Romblon, Romblon",
+    "Roxas City, Capiz",
+    "Roxas, Mindoro",
+    "San Carlos, Negros",
+    "Sibuyan, Romblon",
+    "Siquijor",
+    "Santa Fe, Bantayan Island",
+    "Surigao",
+    "Tagbilaran City, Bohol",
+    "Talibon",
+    "Toledo",
+    "Tubigon",
+    "Ubay, Bohol",
+    "Zamboanqa",
+  ] as string[];
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src='/next.svg'
-          alt='Next.js Logo'
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src='/thirteen.svg' alt='13' width={40} height={31} priority />
-        </div>
-      </div>
+  const ports = portNames.map((name, id) => { return {id, name} as Port; });
 
-      <div className={styles.grid}>
-        <a
-          href='https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-          className={styles.card}
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-          className={styles.card}
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href='https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-          className={styles.card}
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+  return <div>
+    <TripSearchQuery ports={ports} />
+  </div>;
 }
