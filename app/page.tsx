7676e013862e1b82459775186cbb6aef0b9c11a1
Carlos <ngocarlos@gmail.com>
2023-03-10 21:59:00 +0800

    redirect to search results with query params from form

--- app/page.tsx
+++ app/page.tsx
@@ -1,74 +1,57 @@
+'use client';
 import AboutUsCard from '@/common/components/AboutUsCard';
 import TripSearchQuery from '@/common/components/TripSearchQuery';
 import Port from '@/common/models/port';
+import styles from '@/common/components/Header.module.scss';
+import {
+  DEFAULT_NUM_ADULTS,
+  DEFAULT_NUM_CHILDREN,
+  DEFAULT_NUM_INFANTS,
+} from '@/common/constants/form';
+import { Form } from 'antd';
+import { getPorts } from '@/common/services/port.service';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
-  const portNames = [
-    'Ilolo',
-    'Bacolod',
-    'Batangas',
-    'Baybay, Leyte',
-    'Bogo, Cebu',
-    'Bato, Leyte',
-    'Butuan',
-    'Cagayan de Oro',
-    'Calapan',
-    'Calbayog City',
-    'Caticlan',
-    'Cebu',
-    'Consuelo, Camotes',
-    'Danao',
-    'Dapitan',
-    'Dapdap',
-    'Dumaguete',
-    'Dipolog',
-    'Dapa, Siargao',
-    'EB MagaIona,  Negros Occidental',
-    'Estancia',
-    'Getafe',
-    'Guimaras',
-    'Hagnaya',
-    'lligan',
-    'Isabel, Leyte',
-    'Jagna, Bohol',
-    'Medellin, Cebu',
-    'Larena, Siquijor',
-    'Cataingan, Masbate',
-    'Masbate',
-    'Matnog',
-    'Manila',
-    'Nasipit',
-    'Odiongan, Romblon',
-    'Ormoc',
-    'Ozamiz',
-    'Ozamiz',
-    'Palompon',
-    'Plaridel',
-    'Puerto Princesa, Palawan',
-    'Puerto Galera',
-    'Romblon, Romblon',
-    'Roxas City, Capiz',
-    'Roxas, Mindoro',
-    'San Carlos, Negros',
-    'Sibuyan, Romblon',
-    'Siquijor',
-    'Santa Fe, Bantayan Island',
-    'Surigao',
-    'Tagbilaran City, Bohol',
-    'Talibon',
-    'Toledo',
-    'Tubigon',
-    'Ubay, Bohol',
-    'Zamboanqa',
-  ] as string[];
+  const router = useRouter();
+  const [form] = Form.useForm();
 
-  const ports = portNames.map((name, id) => {
-    return { id, name } as Port;
-  });
+  const ports: Port[] = getPorts();
+
+  const redirectToSearchResults = (formValues: any) => {
+    const searchQuery: any = {
+      tripType: formValues.tripType,
+      srcPortId: formValues.srcPortId.toString(),
+      destPortId: formValues.destPortId.toString(),
+      departureDate: formValues.departureDate.format('YYYY-MM-DD'),
+      numAdults: formValues.numAdults.toString(),
+      numChildren: formValues.numChildren.toString(),
+      numInfants: formValues.numInfants.toString(),
+    };
+
+    if (formValues.returnDate) {
+      searchQuery.returnDate = formValues.returnDate.format('YYYY-MM-DD');
+    }
+
+    const queryParams = new URLSearchParams(searchQuery).toString();
+    router.push(`/trips?${queryParams}`);
+  };
 
   return (
     <div>
-      <TripSearchQuery ports={ports} />
+      <Form
+        form={form}
+        className={styles.containerFluid}
+        initialValues={{
+          tripType: 'single',
+          numAdults: DEFAULT_NUM_ADULTS,
+          numChildren: DEFAULT_NUM_CHILDREN,
+          numInfants: DEFAULT_NUM_INFANTS,
+        }}
+        onFinish={redirectToSearchResults}
+      >
+        <TripSearchQuery ports={ports} />
+      </Form>
       <AboutUsCard />
     </div>
   );
---
+++ app/trips/page.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+export default function Trips() {
+  return <div>Trips</div>;
+}
