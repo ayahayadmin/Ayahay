e9a460c33fb6e845b961a7b275a30b3c7395506e
Chenesyy <chesternevansy@gmail.com>
2023-03-11 19:46:33 +0800

    Add trips mock data

---
+++ app/trips/mockData.tsx
@@ -0,0 +1,88 @@
+export const trips = {
+  data: [
+    {
+      availableTrips: [
+        {
+          shippingLine: 'Cokaliong',
+          sourceLoc: 'Cebu',
+          destinationLoc: 'Bacolod',
+          departureDate: '2023-03-20',
+        },
+        {
+          shippingLine: 'Cokaliong',
+          sourceLoc: 'Dumaguete',
+          destinationLoc: 'Ozamiz',
+          departureDate: '2023-03-25',
+        },
+        {
+          shippingLine: 'Cokaliong',
+          sourceLoc: 'Palompon',
+          destinationLoc: 'Iloilo',
+          departureDate: '2023-03-20',
+        },
+        {
+          shippingLine: 'Cokaliong',
+          sourceLoc: 'Palompon',
+          destinationLoc: 'Iloilo',
+          departureDate: '2023-03-25',
+        },
+        {
+          shippingLine: 'Supercat',
+          sourceLoc: 'Cebu',
+          destinationLoc: 'Bacolod',
+          departureDate: '2023-03-25',
+        },
+        {
+          shippingLine: 'Supercat',
+          sourceLoc: 'Iligan',
+          destinationLoc: 'Masbate',
+          departureDate: '2023-03-20',
+        },
+        {
+          shippingLine: 'Supercat',
+          sourceLoc: 'Zamboanga',
+          destinationLoc: 'Nasipit',
+          departureDate: '2023-03-20',
+        },
+        {
+          shippingLine: 'Supercat',
+          sourceLoc: 'Zamboanga',
+          destinationLoc: 'Nasipit',
+          departureDate: '2023-03-25',
+        },
+        {
+          shippingLine: 'Gothong',
+          sourceLoc: 'Cebu',
+          destinationLoc: 'Bacolod',
+          departureDate: '2023-03-25',
+        },
+        {
+          shippingLine: 'Gothong',
+          sourceLoc: 'Cagayan De Oro',
+          destinationLoc: 'Isabel',
+          departureDate: '2023-03-20',
+        },
+      ],
+      page: 1,
+    },
+    {
+      availableTrips: [
+        {
+          shippingLine: 'Gothong',
+          sourceLoc: 'Surigao',
+          destinationLoc: 'Manila',
+          departureDate: '2023-03-25',
+        },
+        {
+          shippingLine: 'Gothong',
+          sourceLoc: 'Surigao',
+          destinationLoc: 'Manila',
+          departureDate: '2023-03-20',
+        },
+      ],
+      page: 2,
+    },
+  ],
+  totalPages: 2,
+  totalItems: 12,
+};
