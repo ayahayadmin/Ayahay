75cac7f7ee2742b116b83f25e7ebce92685e1b3e
Carlos <ngocarlos@gmail.com>
2023-03-12 10:24:50 +0800

    prettify header and footer

--- common/components/Footer.tsx
+++ common/components/Footer.tsx
@@ -8,21 +8,21 @@ import {
 } from '@ant-design/icons';
 import { Col, Input, Row, Space } from 'antd';
 
-const { Search } = Input;
-
 export default function Footer() {
-  const onSearch = (value: string) => console.log(value);
-
   return (
     <footer className={styles['footer']}>
       <div className={styles['cont']}>
         <div className={styles['sub-cont']}>
           <div className={styles['h1']}>Toll Free Customer Care</div>
-          <div className={styles['h1']}><a href="#">+63 999 999 999</a></div>
+          <div className={styles['h1']}>
+            <a href='#'>+63 999 999 999</a>
+          </div>
         </div>
         <div className={styles['sub-cont']}>
           <div className={styles['h1']}>Need live support?</div>
-          <div className={styles['h1']}><a href="#">support@ayahay.com</a></div>
+          <div className={styles['h1']}>
+            <a href='#'>support@ayahay.com</a>
+          </div>
         </div>
         <div className={styles['break']}></div>
         <div className={styles['sub-cont']}>
@@ -33,38 +33,70 @@ export default function Footer() {
         <div className={styles['break']}></div>
         <div className={styles['sub-cont']}>
           <div className={styles['h2']}>Follow us on social media</div>
+          <Space>
+            <FacebookFilled
+              className={styles.icon}
+              style={{ fontSize: '28px' }}
+            />
+            <InstagramFilled
+              className={styles.icon}
+              style={{ fontSize: '28px' }}
+            />
+            <TwitterSquareFilled
+              className={styles.icon}
+              style={{ fontSize: '28px' }}
+            />
+          </Space>
         </div>
       </div>
 
       <div className={styles['cont']}>
         <div className={styles['input-field']}>
           <div className={styles['h2']}>Get Updates & More</div>
-          <input type="email" title="email" placeholder="Your Email"></input>
-          <button type="button">Subscribe</button>
+          <input type='email' title='email' placeholder='Your Email'></input>
+          <button type='button'>Subscribe</button>
         </div>
         <div className={styles['break']}></div>
         <div className={styles['sub-cont']}>
           <div className={styles['h2']}>Company</div>
           <ul>
-            <li><a href="#">About Us</a></li>
-            <li><a href="#">Careers</a></li>
-            <li><a href="#">Press</a></li>
+            <li>
+              <a href='#'>About Us</a>
+            </li>
+            <li>
+              <a href='#'>Careers</a>
+            </li>
+            <li>
+              <a href='#'>Press</a>
+            </li>
           </ul>
         </div>
         <div className={styles['sub-cont']}>
           <div className={styles['h2']}>Support</div>
           <ul>
-            <li><a href="#">Customer Care</a></li>
-            <li><a href="#">Contact</a></li>
-            <li><a href="#">Site Map</a></li>
+            <li>
+              <a href='#'>Customer Care</a>
+            </li>
+            <li>
+              <a href='#'>Contact</a>
+            </li>
+            <li>
+              <a href='#'>Site Map</a>
+            </li>
           </ul>
         </div>
         <div className={styles['sub-cont']}>
           <div className={styles['h2']}>Legal Docs</div>
           <ul>
-            <li><a href="#">Legal Notice</a></li>
-            <li><a href="#">Privacy Policy</a></li>
-            <li><a href="#">Terms and Conditions</a></li>
+            <li>
+              <a href='#'>Legal Notice</a>
+            </li>
+            <li>
+              <a href='#'>Privacy Policy</a>
+            </li>
+            <li>
+              <a href='#'>Terms and Conditions</a>
+            </li>
           </ul>
         </div>
       </div>
@@ -72,11 +104,11 @@ export default function Footer() {
       <div className={styles['cont-bottom']}>
         <span>© 2023 Ayahay All rights reserved.</span>
         <ul>
-            <li>Privacy</li>
-            <li>Terms</li>
-            <li>Site Map</li>
-          </ul>
+          <li>Privacy</li>
+          <li>Terms</li>
+          <li>Site Map</li>
+        </ul>
       </div>
-  </footer>
-  )
+    </footer>
+  );
 }
--- common/components/Header.tsx
+++ common/components/Header.tsx
@@ -3,27 +3,44 @@ import React from 'react';
 import Image from 'next/image';
 import styles from './Header.module.scss';
 import AyahayLogo from '/public/assets/ayahay_logo.png';
+import Link from 'next/link';
 
 export default function Header() {
   return (
     <nav className={styles['nav']}>
       <div className={styles['nav-main']}>
-        <Image src={AyahayLogo} alt="Ayahay Logo" height={80}/>
+        <Image src={AyahayLogo} alt='Ayahay Logo' height={80} />
         <span className={styles['nav-links']}>
           <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Destinations</a></li>
-            <li><a href="#">About Us</a></li>
-            <li><a href="#">My Account</a></li>
-            <li><a href="#">Contact</a></li>
+            <li>
+              <Link href='/'>Home</Link>
+            </li>
+            <li>
+              <Link href='/trips'>Destinations</Link>
+            </li>
+            <li>
+              <a href='#'>About Us</a>
+            </li>
+            <li>
+              <a href='#'>My Account</a>
+            </li>
+            <li>
+              <a href='#'>Contact</a>
+            </li>
           </ul>
         </span>
       </div>
 
       <div className={styles['nav-buttons']}>
-        <a className="button" href="#">Book Now</a>
-        <a className="button" href="#">Sign In</a>
-        <a className="button" href="#">Register</a>
+        <Link href='/trips' className='button'>
+          Book Now
+        </Link>
+        <a className='button' href='#'>
+          Sign In
+        </a>
+        <a className='button' href='#'>
+          Register
+        </a>
       </div>
     </nav>
   );
