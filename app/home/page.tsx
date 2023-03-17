581d5698d63f5c45ca7c0d6a0b570db3e3b81ea4
Chenesyy <chesternevansy@gmail.com>
2023-03-18 02:46:29 +0800

    Remove redundant header & footer

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -4,9 +4,7 @@ import Header from '@/common/components/Header';
 export default function Home() {
   return (
     <div>
-      <Header />
       <h1>Hello Home</h1>
-      <Footer />
     </div>
   );
 }
