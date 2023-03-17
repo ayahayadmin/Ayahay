1d99463a105276f81c08c12bb061a1b3c76964b3
Chenesyy <chesternevansy@gmail.com>
2023-03-18 02:46:38 +0800

    Add enums

---
+++ common/constants/enum.ts
@@ -0,0 +1,11 @@
+export enum SEX {
+  Male = 'Male',
+  Female = 'Female',
+}
+
+export enum CIVIL_STATUS {
+  Single = 'Single',
+  Married = 'Married',
+  Divorced = 'Divorced',
+  Widowed = 'Widowed',
+}
