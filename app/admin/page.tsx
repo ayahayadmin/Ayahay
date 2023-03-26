43a00e0ec1538ab85061cf14dd339283efa3195d
Chenesyy <chesternevansy@gmail.com>
2023-03-26 22:09:22 +0800

    Added Cabin types radio buttons in admin page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,3 +1,54 @@
+'use client';
+import CabinFilter from '@/common/components/form/CabinFilter';
+import AdminSearchQuery from '@/common/models/admin-search-query';
+import {
+  buildAdminSearchQueryFromSearchForm,
+  buildUrlQueryParamsFromAdminSearchForm,
+  initializeAdminSearchFormFromQueryParams,
+} from '@/common/services/search.service';
+import { Form } from 'antd';
+import { debounce } from 'lodash';
+import { useCallback, useEffect, useState } from 'react';
+import TripResults from './tripResults';
+
 export default function Admin() {
-  return <div>Admin Page</div>;
+  const [form] = Form.useForm();
+  const [searchQuery, setSearchQuery] = useState({} as AdminSearchQuery);
+
+  const onPageLoad = () => {
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    const params = Object.fromEntries(urlSearchParams.entries());
+    initializeAdminSearchFormFromQueryParams(form, params);
+    debounceSearch();
+  };
+
+  useEffect(onPageLoad, []);
+
+  const debounceSearch = useCallback(debounce(performSearch, 300), []);
+
+  function performSearch() {
+    const query = buildAdminSearchQueryFromSearchForm(form);
+    setSearchQuery(query);
+    updateUrl();
+    console.log(searchQuery);
+  }
+
+  const updateUrl = () => {
+    const updatedQueryParams = buildUrlQueryParamsFromAdminSearchForm(form);
+    const updatedUrl = `${window.location.origin}${window.location.pathname}?${updatedQueryParams}`;
+    window.history.replaceState({ path: updatedUrl }, '', updatedUrl);
+  };
+
+  return (
+    <Form
+      form={form}
+      onValuesChange={(_, __) => debounceSearch()}
+      onFinish={(_) => debounceSearch()}
+    >
+      <div>
+        <CabinFilter name='cabinTypes' label='Cabin Types' />
+        <TripResults searchQuery={searchQuery} />
+      </div>
+    </Form>
+  );
 }
