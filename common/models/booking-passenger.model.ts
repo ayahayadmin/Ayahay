7e862fa6c53f0f571a60688020e79fbf48841319
Chenesyy <chesternevansy@gmail.com>
2023-04-15 18:04:12 +0800

    Add bookingId; Add different variety of seats; Prettier format

--- common/models/booking-passenger.model.ts
+++ common/models/booking-passenger.model.ts
@@ -1,18 +1,26 @@
-import Booking, { mockBooking } from "./booking.model";
-import Passenger, { mockPassenger } from "./passenger";
-import Seat, { mockSeat } from "./seat.model";
+import Booking, { mockBooking } from './booking.model';
+import Passenger, { mockPassenger } from './passenger';
+import Seat, {
+  mockSeat,
+  mockSeat2,
+  mockSeat3,
+  mockSeat4,
+  mockSeat5,
+} from './seat.model';
 
 export default interface BookingPassenger {
   id: number;
-  booking?: Booking,
-  passenger?: Passenger,
-  seat?: Seat,
+  bookingId: number;
+  booking?: Booking;
+  passenger?: Passenger;
+  seat?: Seat;
   meal: string;
   totalPrice: number;
 }
 
-export const mockBookingPassenger = {
+export const mockBookingPassenger: BookingPassenger = {
   id: 1,
+  bookingId: 1,
   booking: mockBooking,
   passenger: mockPassenger,
   seat: mockSeat,
@@ -24,6 +32,7 @@ export const mockBookingPassengers: BookingPassenger[] = [
   mockBookingPassenger,
   {
     id: 2,
+    bookingId: 2,
     booking: mockBooking,
     passenger: mockPassenger,
     seat: mockSeat,
@@ -32,38 +41,43 @@ export const mockBookingPassengers: BookingPassenger[] = [
   },
   {
     id: 3,
+    bookingId: 3,
     booking: mockBooking,
     passenger: mockPassenger,
-    seat: mockSeat,
+    seat: mockSeat2,
     meal: 'Tapsilog',
     totalPrice: 1000,
   },
   {
     id: 4,
+    bookingId: 4,
     booking: mockBooking,
     passenger: mockPassenger,
-    seat: mockSeat,
+    seat: mockSeat3,
     meal: 'Tapsilog',
     totalPrice: 1000,
   },
   {
     id: 5,
+    bookingId: 5,
     booking: mockBooking,
     passenger: mockPassenger,
-    seat: mockSeat,
+    seat: mockSeat4,
     meal: 'Tapsilog',
     totalPrice: 1000,
   },
   {
     id: 6,
+    bookingId: 6,
     booking: mockBooking,
     passenger: mockPassenger,
-    seat: mockSeat,
+    seat: mockSeat5,
     meal: 'Tapsilog',
     totalPrice: 1000,
   },
   {
     id: 7,
+    bookingId: 7,
     booking: mockBooking,
     passenger: mockPassenger,
     seat: mockSeat,
