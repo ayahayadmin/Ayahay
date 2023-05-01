6064c59e8701cb137d968fe0ec46c4660067c8e2
Chenesyy <chesternevansy@gmail.com>
2023-05-02 00:26:14 +0800

    Add placeholder chart

--- web/app/admin/details/page.tsx
+++ web/app/admin/details/page.tsx
@@ -1,45 +1,90 @@
 'use client';
 import { getTime } from '@/services/search.service';
 import { ITrip, mockTrips } from '@ayahay/models/trip.model';
-import { find, join, split } from 'lodash';
+import { Button } from 'antd';
+import { filter, find, join, split } from 'lodash';
 import React, { useEffect, useState } from 'react';
+import Seats from './seats';
+import styles from './page.module.scss';
+import { getAllBookingsOfTrip } from '@/services/booking.service';
+import LineChart from '@/components/charts/lineChart';
+import { Pie } from 'react-chartjs-2';
+import PieChart from '@/components/charts/pieChart';
+
+const data = {
+  labels: ['Red', 'Blue', 'Yellow'],
+  datasets: [
+    {
+      label: 'My First Dataset',
+      data: [300, 50, 100],
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(54, 162, 235)',
+        'rgb(255, 205, 86)',
+      ],
+      hoverOffset: 4,
+    },
+  ],
+};
 
 export default function Details() {
   const [tripData, setTripData] = useState({} as ITrip);
+  const [seatMapBtnClicked, setSeatMapBtnClicked] = useState(false);
 
   useEffect(() => {
     const urlSearchParams = new URLSearchParams(window.location.search);
     const params = Object.fromEntries(urlSearchParams.entries());
     const fetchTrip = find(mockTrips, { id: Number(params.tripId) })!;
-    console.log(fetchTrip);
+    // const fetchBookings = getAllBookingsOfTrip(fetchTrip?.id);
+    // console.log(fetchBookings);
+
+    // const filteredBookings = filter(fetchBookings, { fetchTrip });
+    // console.log(filteredBookings);
 
     setTripData(fetchTrip);
   }, []);
 
   return (
     <div>
-      {tripData && (
-        <div>
-          <strong>Source:</strong> {tripData.srcPort?.name}
-          <br></br>
-          <strong>Destination:</strong> {tripData.destPort?.name}
-          <br></br>
-          <strong>Departure Date & Time:</strong>{' '}
-          {split(tripData.departureDateIso, 'T')[0]}{' '}
-          {getTime(tripData.departureDateIso)}
-          <br></br>
-          <strong>Trip Type:</strong> {tripData.type}
-          <br></br>
-          <strong>Seats Types:</strong>{' '}
-          {join(tripData.availableSeatTypes, ', ')}
-          <br></br>
-          <strong>Cabin Types:</strong> {join(tripData.availableCabins, ', ')}
-          <br></br>
-          <strong>Meals:</strong> {join(tripData.meals, ', ')}
-          <br></br>
-          <strong>Base:</strong> {tripData.baseFare}
-        </div>
-      )}
+      <div>
+        <h3>Trip Info:</h3>
+        {tripData && (
+          <div>
+            <strong>Source:</strong> {tripData.srcPort?.name}
+            <br></br>
+            <strong>Destination:</strong> {tripData.destPort?.name}
+            <br></br>
+            <strong>Departure Date & Time:</strong>{' '}
+            {split(tripData.departureDateIso, 'T')[0]}{' '}
+            {getTime(tripData.departureDateIso)}
+            <br></br>
+            <strong>Trip Type:</strong> {tripData.type}
+            <br></br>
+            <strong>Seats Types:</strong>{' '}
+            {join(tripData.availableSeatTypes, ', ')}
+            <br></br>
+            <strong>Cabin Types:</strong> {join(tripData.availableCabins, ', ')}
+            <br></br>
+            <strong>Meals:</strong> {join(tripData.meals, ', ')}
+            <br></br>
+            <strong>Base:</strong> {tripData.baseFare}
+          </div>
+        )}
+      </div>
+      <div>
+        <h3>Booking Info:</h3>
+      </div>
+      <div className={styles.chart}>
+        <PieChart data={data} />
+      </div>
+      <Button
+        type='primary'
+        size='large'
+        onClick={() => setSeatMapBtnClicked(true)}
+      >
+        Seat Map
+      </Button>
+      {seatMapBtnClicked && <Seats />}
     </div>
   );
 }
