a69d5c03a6acbd1be7068430706bce2b32c1a4bb
Carlos <ngocarlos@gmail.com>
2023-05-21 13:11:41 +0800

    add admin booking page

---
+++ admin/app/bookings/[id]/page.tsx
@@ -0,0 +1,44 @@
+'use client';
+import styles from './page.module.scss'
+import React, { useEffect, useState } from 'react';
+import BookingPassengersSummary from '@ayahay/components/descriptions/BookingPassengersSummary';
+import { getBookingById } from '@/services/booking.service';
+import { IBooking, mockBooking } from '@ayahay/models/booking.model';
+import { Button, QRCode, Skeleton, Typography } from 'antd';
+import TripSummary from '@ayahay/components/descriptions/TripSummary';
+import { usePathname } from 'next/navigation';
+
+const { Title } = Typography;
+
+export default function GetBooking({ params }) {
+  const [booking, setBooking] = useState<IBooking | undefined>();
+  const onPageLoad = () => {
+    const bookingId = parseInt(params.id);
+    setBooking(getBookingById(bookingId) ?? mockBooking);
+  };
+
+  useEffect(onPageLoad, []);
+
+  const onConfirmAttendance = () => {};
+
+  return (
+    <div className={styles['main-container']}>
+      <Title level={1}>Booking Summary</Title>
+        <div className={styles['top-container']}>
+          <section className={styles['verification-card']}>
+              <Title level={2}>Quick Actions</Title>
+              <Button type='primary' onClick={onConfirmAttendance}>
+                  Confirm Passengers Attendance
+              </Button>
+          </section>
+          <section className={styles['trip-card']}>
+              <Title level={2}>Trip Details</Title>
+              <TripSummary trip={booking?.trip} />
+          </section>
+        </div>
+      <Title level={2}>Passengers</Title>
+      <BookingPassengersSummary booking={booking} />
+    </div>
+    
+  );
+}
