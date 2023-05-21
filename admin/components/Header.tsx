038e7b73134febd43d47db9d912511b5f12e5605
Carlos <ngocarlos@gmail.com>
2023-05-21 13:12:31 +0800

    update admin nav bar

--- admin/components/Header.tsx
+++ admin/components/Header.tsx
@@ -4,17 +4,20 @@ import Image from 'next/image';
 import styles from './Header.module.scss';
 import AyahayLogo from '/public/assets/ayahay-logo.png';
 import Link from 'next/link';
+import { Avatar, Input } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+
+const { Search } = Input;
 
 export default function Header() {
+  const onSearch = (value: string) => console.log(value);
+
   return (
     <nav className={styles['nav-container']}>
       <div className={styles['nav-main']}>
         <Image src={AyahayLogo} alt='Ayahay Logo' height={80} />
         <span className={styles['nav-links']}>
           <ul>
-            <li>
-              <Link href='/'>Scan QR</Link>
-            </li>
             <li>
               <Link href='/schedules'>Schedules</Link>
             </li>
@@ -32,15 +35,12 @@ export default function Header() {
       </div>
 
       <div className={styles['nav-buttons']}>
-        <Link href='/trips' className='button'>
-          Book Now
-        </Link>
-        <a className='button' href='/auth/signIn'>
-          Sign In
-        </a>
-        <a className='button' href='/auth/register'>
-          Register
-        </a>
+        <Search 
+          placeholder="Search for booking..." 
+          onSearch={onSearch} 
+          style={{ width: 200 }} 
+        />
+        <Avatar icon={<UserOutlined />} />
       </div>
     </nav>
   );
