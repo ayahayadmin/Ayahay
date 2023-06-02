e5d2c926c598a4d9c1a05f649e696dfa1682659a
Chenesyy <chesternevansy@gmail.com>
2023-06-03 01:29:04 +0800

    Add content in dashboard

--- admin/app/dashboard/page.tsx
+++ admin/app/dashboard/page.tsx
@@ -1,3 +1,93 @@
+'use client';
+import { mockBookingPassengers, mockBookings } from '@/../packages/models';
+import BarChart from '@/components/charts/barChart';
+import PieChart from '@/components/charts/pieChart';
+import { getAllTrips } from '@/services/trip.service';
+import { DatePicker } from 'antd';
+import { RangePickerProps } from 'antd/es/date-picker';
+import dayjs, { Dayjs } from 'dayjs';
+import { filter, find, includes, map } from 'lodash';
+import { useEffect, useState } from 'react';
+import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
+import isSameOrAfter from 'dayjs/plugin/isSameOrAfter';
+
+const { RangePicker } = DatePicker;
+dayjs.extend(isSameOrBefore);
+dayjs.extend(isSameOrAfter);
+
+const data = {
+  labels: ['Red', 'Blue', 'Yellow'],
+  datasets: [
+    {
+      label: 'My First Dataset',
+      data: [300, 50, 100],
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(54, 162, 235)',
+        'rgb(255, 205, 86)',
+      ],
+      hoverOffset: 4,
+    },
+  ],
+};
+
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  const dateToday = dayjs();
+  const [startMonth, setStartMonth] = useState(
+    dateToday.startOf('month') as Dayjs
+  );
+  const [endMonth, setEndMonth] = useState(dateToday.endOf('month') as Dayjs);
+
+  useEffect(() => {
+    // const bookingPassengers = getBookingPassengersByTripId(tripId); // still waiting for Carlos to update
+    // console.log(bookingPassengers);
+
+    // might want to create a function in .service.tsx, cuz ginagamit din to ni tripList.tsx
+    const trips = filter(getAllTrips(), (trip) => {
+      return (
+        startMonth.isSameOrBefore(trip.departureDateIso) &&
+        endMonth.isSameOrAfter(trip.departureDateIso)
+      );
+    });
+
+    const tripIds = map(trips, (trip) => trip.id);
+    const bookingsTemp = mockBookings.filter((booking) =>
+      includes(tripIds, booking.tripId)
+    );
+
+    const bookingPassengers = map(bookingsTemp, (booking) => {
+      return find(mockBookingPassengers, { bookingId: booking.id });
+    });
+
+    console.log(bookingPassengers);
+    // //TO DO: With bookingPassenger:
+    // - Count how many passengers booked per trip (bar)
+    // - Count how many checked-in (and not) (bar kasama sa bullet 1)
+    // - Count overall passengers given date range (number)
+  }, []);
+
+  const disabledDate: RangePickerProps['disabledDate'] = (current) => {
+    const lastYear = dateToday.subtract(1, 'year');
+    return lastYear && current < lastYear.startOf('day');
+  };
+
+  const onChange: RangePickerProps['onChange'] = (date, dateString) => {
+    setStartMonth(dayjs(dateString[0]).startOf('day'));
+    setEndMonth(dayjs(dateString[1]).endOf('day'));
+  };
+
+  return (
+    <div>
+      <div>
+        <RangePicker
+          defaultValue={[startMonth, endMonth]}
+          disabledDate={disabledDate}
+          onChange={onChange}
+        />
+      </div>
+      <div>
+        <BarChart data={data} />
+      </div>
+    </div>
+  );
 }
