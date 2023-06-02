9ac25c145d131a810ad77f5227e00bfc8fcb9eec
Chenesyy <chesternevansy@gmail.com>
2023-06-03 01:29:15 +0800

    Code cleanup

--- admin/app/trips/page.tsx
+++ admin/app/trips/page.tsx
@@ -11,22 +11,6 @@ import BookingList from './[id]/page';
 import { AdminSearchQuery } from '@ayahay/models/admin-search-query';
 import TripList from './tripList';
 
-const data = {
-  labels: ['Red', 'Blue', 'Yellow'],
-  datasets: [
-    {
-      label: 'My First Dataset',
-      data: [300, 50, 100],
-      backgroundColor: [
-        'rgb(255, 99, 132)',
-        'rgb(54, 162, 235)',
-        'rgb(255, 205, 86)',
-      ],
-      hoverOffset: 4,
-    },
-  ],
-};
-
 export default function Schedules() {
   const [form] = Form.useForm();
   const [searchQuery, setSearchQuery] = useState({} as AdminSearchQuery);
