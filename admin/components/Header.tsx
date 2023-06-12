49fd863b22bb74af26ee7aaef548ad6fe26d52b2
Chenesyy <chesternevansy@gmail.com>
2023-06-12 20:33:36 +0800

    Add notification bell and modal

--- admin/components/Header.tsx
+++ admin/components/Header.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image';
 import styles from './Header.module.scss';
 import AyahayLogo from '/public/assets/ayahay-logo.png';
 import Link from 'next/link';
-import { Avatar, Input } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
+import { Avatar, Button, Input, Modal, Space, notification } from 'antd';
+import { BellOutlined, UserOutlined } from '@ant-design/icons';
 import { useSearchParams } from 'next/navigation';
 
 const { Search } = Input;
@@ -12,6 +12,9 @@ const { Search } = Input;
 export default function Header() {
   const searchParams = useSearchParams();
   const [query, setQuery] = useState('');
+  const [api, contextHolder] = notification.useNotification();
+  const [loading, setLoading] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const onPageLoad = () => {
     const params = Object.fromEntries(searchParams.entries());
@@ -23,6 +26,40 @@ export default function Header() {
 
   const onSearch = (value: string) =>
     window.location.assign(`/search?query=${value}`);
+
+  const showModal = () => {
+    api.destroy();
+    setIsModalOpen(true);
+  };
+
+  const onClickSend = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      setIsModalOpen(false);
+    }, 3000);
+  };
+
+  const onClickCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  const openNotification = () => {
+    const btn = (
+      <Space>
+        <Button type='primary' size='small' onClick={showModal}>
+          Send an Announcement
+        </Button>
+      </Space>
+    );
+    api.open({
+      message: 'Notifications',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      btn,
+      duration: 0,
+    });
+  };
 
   return (
     <nav className={styles['nav-container']}>
@@ -57,6 +94,32 @@ export default function Header() {
           value={query}
           onChange={(e) => setQuery(e.target.value)}
         />
+        {contextHolder}
+        <Button type='text' onClick={openNotification}>
+          <BellOutlined />
+        </Button>
+        <Modal
+          title='Basic Modal'
+          open={isModalOpen}
+          onOk={onClickSend}
+          onCancel={onClickCancel}
+          footer={[
+            <Button key='back' onClick={onClickCancel}>
+              Cancel
+            </Button>,
+            <Button
+              key='submit'
+              type='primary'
+              loading={loading}
+              onClick={onClickSend}
+            >
+              Send
+            </Button>,
+          ]}
+        >
+          <p>Pick trip to send an email</p>
+          <p>Email message body</p>
+        </Modal>
         <Avatar icon={<UserOutlined />} />
       </div>
     </nav>
