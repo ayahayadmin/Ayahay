411e9add7d8eb8134e438cd526a47f225c96cabb
Chenesyy <chesternevansy@gmail.com>
2023-07-09 01:13:15 +0800

    Some modification

--- api/src/trip/trip.controller.ts
+++ api/src/trip/trip.controller.ts
@@ -1,42 +1,39 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { TripService } from './trip.service';
-import { ITrip } from '@ayahay/models';
+import { ITrip, TripSearchDto } from '@ayahay/models';
 
 @Controller('trip')
 export class TripController {
   constructor(private tripService: TripService) {}
 
-  @Get(':tripId')
-  async getTripById(@Param('tripId') tripId: string): Promise<any> {
-    // TO DO:
-    // - missing properties in tripService.trips: departureDateIso, availableSeatTypes, availableCabins, meals
-    return await this.tripService.getTrip({ id: Number(tripId) });
+  @Get()
+  async getAllTrips(@Query() query: TripSearchDto): Promise<any[]> {
+    console.log(query);
+
+    const trips = await this.tripService.getTrips({ where: { id: 1 } }); //id: 1 is just a placeholder || tripType, numAdults, numChildren, numInfants, sort
+
+    return trips;
   }
+
+  // @Get()
+  // async getTripById(@Query() tripId: string): Promise<any> {
+  //   // TO DO:
+  //   // - missing properties in tripService.trips: departureDateIso, availableSeatTypes, availableCabins, meals
+  //   return await this.tripService.getTrip({ id: Number(tripId) });
+  // }
 
   @Get(':referenceNo')
   async getTripByReferenceNo(
     @Param('referenceNo') referenceNo: string
   ): Promise<any> {
     // TO DO:
-    // - missing properties in tripService.trips: departureDateIso, availableSeatTypes, availableCabins, meals
     // - referenceNo is not part of TripWhereUniqueInput
     return await this.tripService.getTrip({ referenceNo });
   }
 
-  @Get()
-  async getAllTrips(): Promise<any[]> {
-    // TO DO:
-    // - missing properties in tripService.trips: departureDateIso, availableSeatTypes, availableCabins, meals
-    // - getAllTrips() will accept parameters. Most prolly it will be these tripType=Single&srcPortId=10&destPortId=0&departureDate=2023-07-02T15%3A29%3A02.341Z&numAdults=1&numChildren=0&numInfants=0&sort=departureDate
-    // - fix values to be passed in this.tripService.trips
-    const trips = await this.tripService.getTrips({ where: { id: 1 } }); //id: 1 is just a placeholder || tripType, numAdults, numChildren, numInfants, sort
-
-    return trips;
-  }
-
   @Post()
   async createTrip(@Body() data: any) {
-    //'any' might change
+    //I think should use ITrip instead of any, but still has errors
     return await this.tripService.createTrip(data);
   }
 }
