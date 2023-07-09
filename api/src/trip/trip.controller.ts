0b972460db922502f7795de014b3795fe7f68fa4
Chenesyy <chesternevansy@gmail.com>
2023-07-09 19:25:58 +0800

    Fix Post; Get all trips almost there

--- api/src/trip/trip.controller.ts
+++ api/src/trip/trip.controller.ts
@@ -1,16 +1,33 @@
 import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { TripService } from './trip.service';
 import { ITrip, TripSearchDto } from '@ayahay/models';
+import { Prisma } from '@prisma/client';
+import { omit } from 'lodash';
 
 @Controller('trip')
 export class TripController {
   constructor(private tripService: TripService) {}
 
   @Get()
-  async getAllTrips(@Query() query: TripSearchDto): Promise<any[]> {
-    console.log(query);
+  async getAllTrips(
+    @Query()
+    query: Omit<
+      TripSearchDto,
+      'tripType' | 'numAdults' | 'numChildren' | 'numInfants'
+    >
+  ): Promise<any[]> {
+    const orderBy = {
+      [query.sort]: 'asc', //ascending by default
+    };
+    const queryWithoutSort = omit(query, 'sort');
 
-    const trips = await this.tripService.getTrips({ where: { id: 1 } }); //id: 1 is just a placeholder || tripType, numAdults, numChildren, numInfants, sort
+    const where = {
+      ...queryWithoutSort,
+      srcPortId: Number(queryWithoutSort.srcPortId),
+      destPortId: Number(queryWithoutSort.destPortId),
+    };
+
+    const trips = await this.tripService.getTrips({ where, orderBy });
 
     return trips;
   }
@@ -32,8 +49,7 @@ export class TripController {
   }
 
   @Post()
-  async createTrip(@Body() data: any) {
-    //I think should use ITrip instead of any, but still has errors
+  async createTrip(@Body() data: Prisma.TripCreateInput) {
     return await this.tripService.createTrip(data);
   }
 }
