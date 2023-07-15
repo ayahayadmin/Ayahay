e6da2b59aa91d3ebd6afc0a00c48232e2697375d
Chenesyy <chesternevansy@gmail.com>
2023-07-15 22:29:49 +0800

    Add bad request exception

--- api/src/search/search.controller.ts
+++ api/src/search/search.controller.ts
@@ -1,4 +1,10 @@
-import { Controller, Get, NotFoundException, Query } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  NotFoundException,
+  Query,
+} from '@nestjs/common';
 import { SearchService } from './search.service';
 import { Booking, Trip } from '@prisma/client';
 
@@ -24,6 +30,10 @@ export class SearchController {
   async getTripsByReferenceNo(
     @Query('referenceNo') referenceNo: string
   ): Promise<Trip[]> {
+    if (!referenceNo) {
+      throw new BadRequestException('Reference Number Cannot Be Empty');
+    }
+
     const trips = await this.searchService.getTripsByReferenceNo({
       where: { referenceNo },
     });
