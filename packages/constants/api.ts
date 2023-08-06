150bff39a522d09dce87d2a455984b7e7c2407b4
Chenesyy <chesternevansy@gmail.com>
2023-08-07 00:00:13 +0800

    Add api endpoints constant

---
+++ packages/constants/api.ts
@@ -0,0 +1,10 @@
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+
+export const ACCOUNT_API = `${BASE_URL}/accounts`;
+export const BOOKING_API = `${BASE_URL}/bookings`;
+export const PASSENGER_API = `${BASE_URL}/passenger`;
+export const PAYMENT_API = `${BASE_URL}/pay`;
+export const PORTS_API = `${BASE_URL}/ports`;
+export const SEARCH_API = `${BASE_URL}/search`;
+export const SHIPPING_LINE_API = `${BASE_URL}/shipping-lines`;
+export const TRIP_API = `${BASE_URL}/trips`;
--- packages/constants/index.ts
+++ packages/constants/index.ts
@@ -1,3 +1,4 @@
 export * from './default';
 export * from './enum';
 export * from './limits';
+export * from './api';
