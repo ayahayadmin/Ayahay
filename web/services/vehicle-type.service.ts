77ca20225cedbbfdcf465625dfcabfa325ab15de
Chenesyy <chesternevansy@gmail.com>
2023-09-10 18:35:22 +0800

    Refetch vehicle types

--- web/services/vehicle-type.service.ts
+++ web/services/vehicle-type.service.ts
@@ -4,21 +4,24 @@ import axios from 'axios';
 import dayjs from 'dayjs';
 import { isWithinTimeInterval, removeCache } from './utils';
 
+async function fetchAndCacheVehicleTypes(): Promise<IVehicleType[]> {
+  const { data } = await axios.get(`${VEHICLE_TYPES_API}`);
+  localStorage.setItem(
+    'vehicle-types',
+    JSON.stringify({ data, timestamp: dayjs() })
+  );
+  return data;
+}
+
 export async function getVehicleTypes(): Promise<IVehicleType[]> {
   const vehicleTypesJson = localStorage.getItem('vehicle-types');
   if (vehicleTypesJson === undefined || vehicleTypesJson === null) {
-    const { data } = await axios.get(`${VEHICLE_TYPES_API}`);
-    localStorage.setItem(
-      'vehicle-types',
-      JSON.stringify({ data, timestamp: dayjs() })
-    );
-    return data;
+    return fetchAndCacheVehicleTypes();
   }
 
   const { data, timestamp } = JSON.parse(vehicleTypesJson);
   if (!isWithinTimeInterval(timestamp)) {
-    removeCache('vehicle-types');
-    //re-fetch vehicle-types?
+    return fetchAndCacheVehicleTypes();
   }
   return data;
 }
