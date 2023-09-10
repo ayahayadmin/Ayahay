af4eb887bf319b94de1824f3a47c194e7835c44c
Chenesyy <chesternevansy@gmail.com>
2023-09-10 18:36:30 +0800

    Refetch cabin types

--- web/services/cabin-type.service.ts
+++ web/services/cabin-type.service.ts
@@ -4,21 +4,24 @@ import axios from 'axios';
 import dayjs from 'dayjs';
 import { isWithinTimeInterval, removeCache } from './utils';
 
+async function fetchAndCacheCabinTypes(): Promise<ICabinType[]> {
+  const { data } = await axios.get(`${CABIN_TYPES_API}`);
+  localStorage.setItem(
+    'cabin-types',
+    JSON.stringify({ data, timestamp: dayjs() })
+  );
+  return data;
+}
+
 export async function getCabinTypes(): Promise<ICabinType[]> {
   const cabinTypesJson = localStorage.getItem('cabin-types');
   if (cabinTypesJson === undefined || cabinTypesJson === null) {
-    const { data } = await axios.get(`${CABIN_TYPES_API}`);
-    localStorage.setItem(
-      'cabin-types',
-      JSON.stringify({ data, timestamp: dayjs() })
-    );
-    return data;
+    return fetchAndCacheCabinTypes();
   }
 
   const { data, timestamp } = JSON.parse(cabinTypesJson);
   if (!isWithinTimeInterval(timestamp)) {
-    removeCache('cabin-types');
-    //re-fetch cabin-types?
+    return fetchAndCacheCabinTypes();
   }
   return data;
 }
