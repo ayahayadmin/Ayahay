e2dac5aec91595d1829caa254b765b365db5d982
Chenesyy <chesternevansy@gmail.com>
2023-09-10 18:33:49 +0800

    Refetch shipping line

--- web/services/shipping-line.service.ts
+++ web/services/shipping-line.service.ts
@@ -4,21 +4,24 @@ import axios from 'axios';
 import dayjs from 'dayjs';
 import { isWithinTimeInterval, removeCache } from './utils';
 
+async function fetchAndCacheShippingLines(): Promise<IShippingLine[]> {
+  const { data } = await axios.get(`${SHIPPING_LINE_API}`);
+  localStorage.setItem(
+    'shipping-lines',
+    JSON.stringify({ data, timestamp: dayjs() })
+  );
+  return data;
+}
+
 export async function getShippingLines(): Promise<IShippingLine[]> {
   const shippingLinesJson = localStorage.getItem('shipping-lines');
   if (shippingLinesJson === undefined || shippingLinesJson === null) {
-    const { data } = await axios.get(`${SHIPPING_LINE_API}`);
-    localStorage.setItem(
-      'shipping-lines',
-      JSON.stringify({ data, timestamp: dayjs() })
-    );
-    return data;
+    return fetchAndCacheShippingLines();
   }
 
   const { data, timestamp } = JSON.parse(shippingLinesJson);
   if (!isWithinTimeInterval(timestamp)) {
-    removeCache('shipping-lines');
-    //re-fetch shipping-lines?
+    return fetchAndCacheShippingLines();
   }
   return data;
 }
