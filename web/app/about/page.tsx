9c257878ba2a689a960f99285c11be91810b4650
Chenesyy <chesternevansy@gmail.com>
2023-09-22 16:47:30 +0800

    Extract card into a component

--- web/app/about/page.tsx
+++ web/app/about/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { Card, Typography } from 'antd';
 import styles from './page.module.scss';
+import ProfileCard from '@/components/about/Card';
 
 const { Title } = Typography;
 
@@ -9,198 +10,95 @@ export default function AboutUs() {
     <div className={styles['main-container']}>
       <Title level={3}>Who Are We</Title>
       <div className={styles['cards']}>
-        <Card
-          hoverable
-          className={styles['card']}
-          cover={
-            <img
-              alt='example'
-              className={styles['image']}
-              src='/assets/jay-engalla.jpg'
-            />
-          }
-          onClick={() => {
-            window.open(
-              'https://www.linkedin.com/in/jay-mark-engalla-537210b8/',
-              '_blank'
-            );
-          }}
-        >
-          <p>
-            <strong>Jay Mark Engalla</strong>
-          </p>
-          <p>Chief Executive Officer</p>
-        </Card>
-        <Card
-          hoverable
-          className={styles['card']}
-          cover={
-            <img
-              alt='example'
-              className={styles['image']}
-              src='/assets/josef-arroyo.jpg'
-            />
-          }
-          onClick={() => {
-            window.open(
-              'https://www.linkedin.com/in/josef-michael-arroyo-jmja007/',
-              '_blank'
-            );
-          }}
-        >
-          <p>
-            <strong>Josef Michael Arroyo</strong>
-          </p>
-          <p>Chief Operating Officer</p>
-        </Card>
-        <Card
-          hoverable
-          className={styles['card']}
-          cover={
-            <img
-              alt='example'
-              className={styles['image']}
-              src='/assets/tonee-atil.jpg'
-            />
-          }
-          onClick={() => {
-            window.open(
-              'https://www.linkedin.com/in/tonee-pacres-atil-92b3a3292/',
-              '_blank'
-            );
-          }}
-        >
-          <p>
-            <strong>Tonee Pacres-Atil</strong>
-          </p>
-          <p>Chief Financial Officer</p>
-        </Card>
-        {/* </div>
-      <div className={styles['cards']}> */}
-        <Card
-          hoverable
-          className={styles['card']}
-          cover={
-            <img
-              alt='example'
-              className={styles['image']}
-              src='/assets/james-atil.jpg'
-            />
-          }
-          onClick={() => {
-            window.open('https://www.linkedin.com/in/jamesatil/', '_blank');
-          }}
-        >
-          <p>
-            <strong>James Atil</strong>
-          </p>
-          <p>Chief Risk Officer</p>
-        </Card>
-        <Card
-          hoverable
-          className={styles['card']}
-          cover={
-            <img
-              alt='example'
-              className={styles['image']}
-              src='/assets/carlos-ngo.jpg'
-            />
-          }
-          onClick={() => {
-            window.open('https://www.linkedin.com/in/carlosngo/', '_blank');
-          }}
-        >
-          <p>
-            <strong>Carlos Ngo</strong>
-          </p>
-          <p>Chief Technology Officer</p>
-        </Card>
-        <Card
-          hoverable
-          className={styles['card']}
-          cover={
-            <img
-              alt='example'
-              className={styles['image']}
-              src='/assets/chester-sy.jpg'
-            />
-          }
-          onClick={() => {
-            window.open(
-              'https://www.linkedin.com/in/chester-nevan-sy/',
-              '_blank'
-            );
-          }}
-        >
-          <p>
-            <strong>Chester Nevan Sy</strong>
-          </p>
-          <p>Chief Product Officer</p>
-        </Card>
+        <ProfileCard
+          srcImage='/assets/jay-engalla.jpg'
+          linkedinUrl='https://www.linkedin.com/in/jay-mark-engalla-537210b8/'
+          name='Jay Mark Engalla'
+          position='Chief Executive Officer'
+        />
+        <ProfileCard
+          srcImage='/assets/josef-arroyo.jpg'
+          linkedinUrl='https://www.linkedin.com/in/josef-michael-arroyo-jmja007/'
+          name='Josef Michael Arroyo'
+          position='Chief Operating Officer'
+        />
+        <ProfileCard
+          srcImage='/assets/tonee-atil.jpg'
+          linkedinUrl='https://www.linkedin.com/in/tonee-pacres-atil-92b3a3292/'
+          name='Tonee Pacres-Atil'
+          position='Chief Financial Officer'
+        />
+        <ProfileCard
+          srcImage='/assets/james-atil.jpg'
+          linkedinUrl='https://www.linkedin.com/in/jamesatil/'
+          name='James Atil'
+          position='Chief Risk Officer'
+        />
+        <ProfileCard
+          srcImage='/assets/carlos-ngo.jpg'
+          linkedinUrl='https://www.linkedin.com/in/carlosngo/'
+          name='Carlos Ngo'
+          position='Chief Technology Officer'
+        />
+        <ProfileCard
+          srcImage='/assets/chester-sy.jpg'
+          linkedinUrl='https://www.linkedin.com/in/chester-nevan-sy/'
+          name='Chester Nevan Sy'
+          position='Chief Product Officer'
+        />
       </div>
       <Title level={3} className={styles['title']}>
         About Us
       </Title>
-      <div>
-        <p>
-          Welcome to Ayahay, where innovation meets expertise in the world of
-          logistics and technology. We are a dynamic and forward-thinking
-          company founded by individuals with a wealth of experience in the IT
-          and logistics industry. Our journey began with a shared vision: to
-          revolutionize the way logistics and transportation are managed and
-          experienced in the Philippines.
-        </p>
-      </div>
+      <p>
+        Welcome to Ayahay, where innovation meets expertise in the world of
+        logistics and technology. We are a dynamic and forward-thinking company
+        founded by individuals with a wealth of experience in the IT and
+        logistics industry. Our journey began with a shared vision: to
+        revolutionize the way logistics and transportation are managed and
+        experienced in the Philippines.
+      </p>
       <Title level={3} className={styles['title']}>
         Our Story
       </Title>
-      <div>
-        <p>
-          Ayahay was born out of a deep understanding of the challenges faced by
-          the shipping and logistics industry in adapting to the digital age.
-          The founders of Ayahay brought together their extensive knowledge and
-          experience in IT and logistics to create a solution that would bridge
-          the gap between traditional practices and modern technology.
-        </p>
-      </div>
+      <p>
+        Ayahay was born out of a deep understanding of the challenges faced by
+        the shipping and logistics industry in adapting to the digital age. The
+        founders of Ayahay brought together their extensive knowledge and
+        experience in IT and logistics to create a solution that would bridge
+        the gap between traditional practices and modern technology.
+      </p>
       <Title level={3} className={styles['title']}>
         Expertise in IT Logistics
       </Title>
-      <div>
-        <p>
-          Our team comprises experts who have spent years working at the
-          intersection of information technology and logistics. We understand
-          the intricacies of supply chain management, fleet operations, and
-          passenger services. This expertise has allowed us to design and
-          develop innovative solutions that address the unique needs of the
-          logistics and shipping industry.
-        </p>
-      </div>
+      <p>
+        Our team comprises experts who have spent years working at the
+        intersection of information technology and logistics. We understand the
+        intricacies of supply chain management, fleet operations, and passenger
+        services. This expertise has allowed us to design and develop innovative
+        solutions that address the unique needs of the logistics and shipping
+        industry.
+      </p>
       <Title level={3} className={styles['title']}>
         Mission
       </Title>
-      <div>
-        <p>
-          Our mission is to empower businesses in the shipping line industry to
-          adapt to advanced technology and digitization. We aim to make it
-          easier for businesses to manage their fleets and enhance the sea
-          travel experience for passengers, saving them time and ensuring a
-          hassle-free journey.
-        </p>
-      </div>
+      <p>
+        Our mission is to empower businesses in the shipping line industry to
+        adapt to advanced technology and digitization. We aim to make it easier
+        for businesses to manage their fleets and enhance the sea travel
+        experience for passengers, saving them time and ensuring a hassle-free
+        journey.
+      </p>
       <Title level={3} className={styles['title']}>
         Vision
       </Title>
-      <div>
-        <p>
-          At Ayahay, we envision a future where the shipping line industry in
-          the Philippines fully embraces technology, becoming digitally
-          connected and customer-centric. We aim to lead the way in
-          revolutionizing sea travel by providing a user-friendly platform that
-          simplifies booking, enhances communication, and ensures stress-free
-          journeys for passengers.
-        </p>
-      </div>
+      <p>
+        At Ayahay, we envision a future where the shipping line industry in the
+        Philippines fully embraces technology, becoming digitally connected and
+        customer-centric. We aim to lead the way in revolutionizing sea travel
+        by providing a user-friendly platform that simplifies booking, enhances
+        communication, and ensures stress-free journeys for passengers.
+      </p>
     </div>
   );
 }
---
+++ web/components/about/Card.tsx
@@ -0,0 +1,32 @@
+import { Card } from 'antd';
+import styles from './Card.module.scss';
+
+interface ProfileCardProps {
+  srcImage: string;
+  linkedinUrl: string;
+  name: string;
+  position: string;
+}
+
+export default function ProfileCard({
+  srcImage,
+  linkedinUrl,
+  name,
+  position,
+}: ProfileCardProps) {
+  return (
+    <Card
+      hoverable
+      className={styles['card']}
+      cover={<img alt='example' className={styles['image']} src={srcImage} />}
+      onClick={() => {
+        window.open(linkedinUrl, '_blank');
+      }}
+    >
+      <p>
+        <strong>{name}</strong>
+      </p>
+      <p>{position}</p>
+    </Card>
+  );
+}
