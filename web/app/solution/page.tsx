a281f9bce8eb577c3436cbc6a9875c1e9ad5ab90
Chenesyy <chesternevansy@gmail.com>
2023-09-23 18:48:13 +0800

    Solutions page done

---
+++ web/app/solution/page.tsx
@@ -0,0 +1,30 @@
+'use client';
+import { Typography } from 'antd';
+import styles from './page.module.scss';
+
+const { Title } = Typography;
+
+export default function Solution() {
+  return (
+    <div className={styles['main-container']}>
+      <Title level={1} style={{ fontSize: 30 }}>
+        Solution for Passengers & Shippers
+      </Title>
+      <p>Booking Platform</p>
+      <img
+        className={styles['image']}
+        alt='booking'
+        src='/assets/booking-platform.png'
+      />
+      <Title level={1} style={{ fontSize: 30, paddingTop: 15 }}>
+        Solution for Ocean Liners
+      </Title>
+      <p>Data Dashboard</p>
+      <img
+        className={styles['image']}
+        alt='dashboard'
+        src='/assets/data-dashboard.png'
+      />
+    </div>
+  );
+}
