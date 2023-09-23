ad69e65a2905bbd050e7cbea41d87b0f81f408ad
Chenesyy <chesternevansy@gmail.com>
2023-09-23 19:37:56 +0800

    Resouce /blogs and /vlogs done

---
+++ web/app/resources/blogs/page.tsx
@@ -0,0 +1,20 @@
+'use client';
+import { Typography } from 'antd';
+import styles from './page.module.scss';
+
+const { Title } = Typography;
+
+export default function Blogs() {
+  return (
+    <div className={styles['main-container']}>
+      <Title level={1} style={{ fontSize: 30, marginBottom: 5 }}>
+        Talks about how we closed the deal with Aznar
+      </Title>
+      <img
+        className={styles['image']}
+        alt='aznar deal'
+        src='/assets/aznar-deal.jpg'
+      />
+    </div>
+  );
+}
