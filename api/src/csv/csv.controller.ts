8fb1adcc7594ab50b9e4d91469664a9dfc600b7c
Chester Sy <chesternevansy@gmail.com>
2023-11-03 12:14:28 +0800

    CSV creation transfer to FE (#57)

--- api/src/csv/csv.controller.ts
+++ api/src/csv/csv.controller.ts
@@ -12,16 +12,8 @@ import {
 import { CsvService } from './csv.service';
 import { AuthGuard } from 'src/guard/auth.guard';
 import { Roles } from 'src/decorator/roles.decorator';
-import { Blob } from 'buffer';
 
 @Controller('csv')
 export class CsvController {
   constructor(private csvService: CsvService) {}
-
-  @Post()
-  @UseGuards(AuthGuard)
-  @Roles('SuperAdmin', 'Admin', 'Staff')
-  async generateBookingCsv(@Body() bookings: any[]) {
-    return await this.csvService.generateBookingCsv(bookings);
-  }
 }
