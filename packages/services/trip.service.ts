067eeed3bd691c41b33baa2a24faff568676db8b
Carlos <ngocarlos@gmail.com>
2024-01-23 12:57:24 +0800

    properly fetch associated entities to trips

--- packages/services/trip.service.ts
+++ packages/services/trip.service.ts
@@ -1,20 +1,26 @@
 import { ITrip } from '@ayahay/models';
-import { getPort } from './port.service';
-import { getShippingLine } from './shipping-line.service';
+import { getPort, getPorts } from './port.service';
+import { getShippingLine, getShippingLines } from './shipping-line.service';
 
-export function mapTripResponseData(responseData: ITrip) {
-  return Promise.allSettled([
-    // For now we are just interested with Ports & Shipping Line. In the future we can add more like Ship
-    getPort(responseData.srcPortId),
-    getPort(responseData.destPortId),
-    getShippingLine(responseData.shippingLineId),
-  ]).then(([srcPort, destPort, shippingLine]) => {
-    return {
-      ...responseData,
-      srcPort: srcPort.status === 'fulfilled' ? srcPort.value : '',
-      destPort: destPort.status === 'fulfilled' ? destPort.value : '',
-      shippingLine:
-        shippingLine.status === 'fulfilled' ? shippingLine.value : '',
-    };
-  });
+export async function fetchAssociatedEntitiesToTrips(
+  trips: ITrip[]
+): Promise<void> {
+  // this ensures all ports and shipping lines are cached before a getById is called
+  // without this, the parallel call to getById will all fetch getAll at the same time for N times
+  await Promise.allSettled([getPorts(), getShippingLines()]);
+  await Promise.all(trips.map((trip) => fetchAssociatedEntitiesToTrip(trip)));
+}
+
+export async function fetchAssociatedEntitiesToTrip(
+  trip: ITrip
+): Promise<void> {
+  const [srcPort, destPort, shippingLine] = await Promise.allSettled([
+    getPort(trip.srcPortId),
+    getPort(trip.destPortId),
+    getShippingLine(trip.shippingLineId),
+  ]);
+  trip.srcPort = srcPort.status === 'fulfilled' ? srcPort.value : undefined;
+  trip.destPort = destPort.status === 'fulfilled' ? destPort.value : undefined;
+  trip.shippingLine =
+    shippingLine.status === 'fulfilled' ? shippingLine.value : undefined;
 }
